8328f3eceed5b5e0d35b9541f7b8969e5bc9a017
Dhruv Rekhawat <110072380+DhruvRekhawat@users.noreply.github.com>
2024-06-08 22:37:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const inputTask = document.getElementById('input-task')
+const addTaskBtn = document.getElementById('addTaskBtn')
+const deleteTaskBtn = document.getElementById('deleteTaskBtn')
+const taskListEl = document.getElementById('taskItems')
+
+let tasksList = []
+
+
+function renderList(){
+    taskListEl.innerHTML = ""
+    for (let i =0;i<=tasksList.length-1;i++){
+    let newTask = document.createElement('li') //<li></li>
+    newTask.innerText = tasksList[i] // <li>tdu</li>
+    taskListEl.appendChild(newTask)// ol --> li
+   }
+
+}
+
+
+function addTask(){
+   tasksList.push(inputTask.value)
+   renderList()
+}
+function deleteTask(){
+    tasksList.pop()
+    renderList()
+}
+addTaskBtn.addEventListener('click',addTask)
+deleteTaskBtn.addEventListener('click',deleteTask)
